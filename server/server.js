917df0d068d471a23a4d63a3ff89832bda2eb1f4
Cristobal Barberis <cristobal@barberis.com>
2022-10-29 20:39:27 -0400

    rest version

--- server/server.js
+++ server/server.js
@@ -10,9 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(routes);
 
